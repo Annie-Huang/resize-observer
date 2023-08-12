bfbe63589c3f6deced199fbb919d440ad55ebaa3
Annie Huang <annie.hn@gmail.com>
2023-08-12 11:28:04 +1000

    Observe both box and container in script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+const box = document.querySelector('.box');
+const container = document.querySelector('.container');
+
+const observer = new ResizeObserver((entries) => {
+  console.log(entries);
+});
+
+observer.observe(box);
+observer.observe(container);
