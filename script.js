30a9f266900ffb24c22a7a55591bb6b6e4b309f2
Annie Huang <annie.hn@gmail.com>
2023-08-12 11:32:11 +1000

    Add notes

--- script.js
+++ script.js
@@ -1,6 +1,28 @@
 const box = document.querySelector('.box');
 const container = document.querySelector('.container');
 
+/*
+Each ResizeObserverEntry has the following attributes {
+  borderBoxSize: [{
+    blockSize
+    inlineSize
+  }]
+  contentBoxSize
+  contentRect: {     <----- actual shape of the element
+    bottom
+    height
+    left
+    right
+    top
+    width
+    x
+    y
+  }
+  devicePixelContentBoxSize
+  target: div.box          !!! <----- The actual thing we are observing
+}
+*/
+
 const observer = new ResizeObserver((entries) => {
   console.log(entries);
 });
