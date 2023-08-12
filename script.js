6aabbaeac76db18d79b981ff9ca3714025c71066
Annie Huang <annie.hn@gmail.com>
2023-08-12 11:46:02 +1000

    Add notes

--- script.js
+++ script.js
@@ -23,6 +23,12 @@ Each ResizeObserverEntry has the following attributes {
 }
 */
 
+// The ResizeObserver is quite performant because it's doing all the checks internally inside
+// your browser and you don't have to worry about doing polling or a bunch of like window resize event
+//
+// It is useful because sometimes your element resize not triggered by you resize the screen,
+// e.g. when you add things into a editable content that cause the sibling box to shrink.
+// In such cause the ResizeObserver is the perfect solution
 const observer = new ResizeObserver((entries) => {
   console.log(entries);
 
